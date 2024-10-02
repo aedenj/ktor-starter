5199cadfa8d7be259c415de670433c6cfaf10121
Aeden Jameson <aeden.jameson@gmail.com>
2024-10-02 16:19:46 -0700

    combine tests

--- app/src/test/kotlin/com/example/E2EServiceReadinessTest.kt
+++ app/src/test/kotlin/com/example/E2EServiceReadinessTest.kt
@@ -2,9 +2,13 @@ package com.example
 
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.hostIsIp
+import io.ktor.server.application.pluginOrNull
+import io.ktor.server.routing.IgnoreTrailingSlash
+import io.ktor.server.testing.testApplication
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -13,6 +17,15 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
+/**
+ * The general goal of this system readiness test is to ensure that the system is ready to accept traffic. Revealing
+ * simple failures severe enough to reject a prospective change in the context of all requisite components and dependencies.
+ * The types of changes a readiness test can catch include:
+ *
+ * - Missing configuration
+ * - Incorrect configuration to dependencies
+ * - Missing service features. (e.g. Compression, DefaultHeaders, IgnoreTrailingSlash, etc.)
+ */
 @DisplayName("e2e system readiness test")
 @Testcontainers
 class E2EServiceReadinessTest {
@@ -43,4 +56,13 @@ class E2EServiceReadinessTest {
             statusCode(HttpStatusCode.OK.value)
         }
     }
+
+    @Test
+    @DisplayName("ignores trailing slash in all paths")
+    fun testIgnoreTrailingSlash() =
+        testApplication {
+            application {
+                assertThat(pluginOrNull(IgnoreTrailingSlash)).isNotNull()
+            }
+        }
 }

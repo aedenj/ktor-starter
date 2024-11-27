6c4b3372e2349a8bc9c8029890a4552b1d0f82ee
Aeden Jameson <aeden.jameson@gmail.com>
2024-11-27 15:14:10 -0800

    remove old infra terragrunt

--- infra/aws/app/infra/ecr/repository/terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "registry_id" {
-  description = "The registry ID of the repository"
-  value = module.ecr.repository_registry_id
-}
--- infra/aws/app/infra/ecr/secret/terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "secret_name" {
-  description = "The name of the secret"
-  value = var.cluster_name
-}
